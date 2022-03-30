0076aef4eebdee7e686f37c72423c44a27d13047
Sofie Berger <94051947+SofieBerger@users.noreply.github.com>
2022-03-30 14:17:08 +0200

    Add files via upload

--- opdracht2/scripts/script.js
+++ opdracht2/scripts/script.js
@@ -1,12 +1,32 @@
-// JavaScript Document
-console.log("howdy");
 
+//API//
 const URL = "https://uselessfacts.jsph.pl//random.json?language=en";
 
 //haal de lijst 'ul' op uit de html
 const list = document.querySelector('ul.api');
 //haal de button op uit de html
 const buttonToverstok = document.querySelector('button.toverstok');
+
+
+/********************************/
+/* Nummer opgeslagen favorieten */
+/********************************/
+function calcFavNum (){
+	const favAr = document.getElementsByClassName("favItem");
+		// console.log('favAr length' + favAr.length);
+		var numberFav = document.querySelector(".numberFav"); 
+		numberFav.innerHTML = favAr.length; //
+	
+}
+
+/***********************/
+/* Verwijder van lijst */
+/***********************/
+function removeMe(elem){
+	document.getElementById("myList1").removeChild(elem.parentNode);
+	calcFavNum(); //function van de teller opnieuw uitvoeren
+}
+
 
 /****************/
 /* VUL DE LIJST */
@@ -20,7 +40,7 @@ function getFacts() {
 					const factsHTML =
 						`
 						<li>
-							<p>${data.text}</p>
+							<p class="textApi">${data.text}</p>
 						</li>
 						`;
 					
@@ -46,19 +66,49 @@ async function getData(URL) {
 /*********/
 /* START */
 /*********/
-// getAmiibo();
-
 buttonToverstok.addEventListener("click", getFacts);
 
 
-
-
-// hamburger menu
-var buttonSter = document.querySelector("button.favorieten");
+/******************/
+/* Hamburger menu */
+/******************/
+var buttonStar = document.querySelector("button.star");
 
 function menuOpenDicht(){
-	var favorietenLijst = document.querySelector("ul.favorieten");
+	var favorietenLijst = document.querySelector("section.favorieten");
 	favorietenLijst.classList.toggle("toonMenu");
 }
 
-buttonSter.addEventListener("click", menuOpenDicht);
+buttonStar.addEventListener("click", menuOpenDicht);
+
+
+/******************/
+/* Clone API */
+/******************/
+var addToFavesBtn = document.querySelector(".addToFaves");
+addToFavesBtn.addEventListener("click", () => { //anonieme functie
+
+	const myNode = document.querySelector(".textApi"); //tekst van de api ophalen
+	const liElement = document.createElement("li"); //maakt een li aan
+		liElement.classList.add("favItem")
+
+	liElement.innerHTML = myNode.textContent + '<button class="removeBtn" onclick="removeMe(this)"> Remove </button>'; //veranderd html 
+
+	document.getElementById("myList1").appendChild(liElement); //zet li element in favorieten lijst
+ 		calcFavNum ();
+
+});
+
+/******************/
+/* Drag & drop */
+/******************/
+var options = {
+	// de tijd van swappen in ms
+	animation: 1000
+  }
+  
+  /* het daadwerkelijk initialiseren van het draggen-en-droppen */
+  // list - de ID van de ul
+  // options - hierboven gedefinieerd
+  var deLijst = document.getElementById('myList1');
+  var sortable = Sortable.create(deLijst, options);
